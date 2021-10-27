f27469266fbd20278ebde8dc739326b7fa7cb7d3
ZhannaKarpova <92864174+ZhannaKarpova@users.noreply.github.com>
2021-10-27 16:21:40 +0700

    Добавьте файлы проекта.

---
+++ Lab5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                {
+                    int n;
+                    Console.WriteLine("введите семь чисел  n");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    int[] array = new int[6];
+                    float summa = 0;
+                    for (int i=0;i<array.Length; i++)
+                    {
+                        array[i] = Convert.ToInt32(Console.ReadLine());
+                        summa += array[i];
+                    }
+                    Console.Write("среднее значение:"+summa/6);
+                    Console.WriteLine();
+
+
+                }
+
+                Console.ReadKey();
+
+
+            }
+
+        }
+    }
+}
+
